95cf97affc946875162ff2cb045c585dafd123c1
Sara Charron-Milnikel <sjmilnikel@yahoo.com>
2023-02-12 05:27:38 +0000

    MTC cont.

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,10 +1,4 @@
-#Output the IP Address of the Container
-output "container-name" {
-    value = module.container[*].container-name
-    description = "The name of the container"
-}
-
-output "ip-address" {
-    value = flatten(module.container[*].ip-address)
-    description = "The IP address of the container"
+output "application_access" {
+  value       = [for x in module.container[*] : x]
+  description = "The name and socket for each application."
 }
