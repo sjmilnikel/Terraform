74a61a3597b4da25437adb90b6bcafd886fa435e
Sara Charron-Milnikel <sjmilnikel@yahoo.com>
2023-03-08 05:52:03 +0000

    project cont.

--- terraform-2tier-project/main.tf
+++ terraform-2tier-project/main.tf
@@ -4,15 +4,12 @@ module "vpc" {
   source           = "./vpc"
   vpc_cidr         = "10.100.0.0/16"
   public_sn_count  = 2
-  private_sn_count = 2
+  private_sn_count = 4
   public_cidrs     = [for i in range(2, 255, 2) : cidrsubnet("10.100.0.0/16", 8, i)]
   private_cidrs    = [for i in range(1, 255, 2) : cidrsubnet("10.100.0.0/16", 8, i)]
 }
 
-module "ec2" {
-  source = "git::https://github.com/sjmilnikel/Terraform_Project.git"
-}
-
-# module "ec2_clone" {
-#   source = "git@github.com:sjmilnikel/Terraform_Project.git"
+# module "ec2" {
+#   source = 
 # }
+
--- terraform-2tier-project/vpc/main.tf
+++ terraform-2tier-project/vpc/main.tf
@@ -15,6 +15,9 @@ resource "aws_vpc" "my_2tier_vpc" {
   tags = {
     Name = "my_2tier-${random_integer.random.id}"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_subnet" "my_public_subnet" {
@@ -29,6 +32,13 @@ resource "aws_subnet" "my_public_subnet" {
   }
 }
 
+resource "aws_route_table_association" "my_2tier_public_assoc" {
+  count          = var.public_sn_count
+  subnet_id      = aws_subnet.my_public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.my_2tier_route_table.id
+}
+
+
 resource "aws_subnet" "my_private_subnet" {
   count             = var.private_sn_count
   vpc_id            = aws_vpc.my_2tier_vpc.id
@@ -37,6 +47,36 @@ resource "aws_subnet" "my_private_subnet" {
 
   tags = {
     Name = "my_private_${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "my_2tier_gateway" {
+  vpc_id = aws_vpc.my_2tier_vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "my_2tier_route_table" {
+  vpc_id = aws_vpc.my_2tier_vpc.id
+
+  tags = {
+    Name = "my_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.my_2tier_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_2tier_gateway.id
+}
+
+resource "aws_default_route_table" "my_2tier_route_table" {
+  default_route_table_id = aws_vpc.my_2tier_vpc.default_route_table_id
+
+  tags = {
+    Name = "my_private_rt"
   }
 }
 
